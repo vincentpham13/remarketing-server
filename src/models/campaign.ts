ce833ee29d008e43aeb0e7e434dc24b5c5aac926
Vincent Pham <vincent.pham013@gmail.com>
2021-01-25 10:38:56 +0700

    create api for campaign

--- src/models/campaign.ts
+++ src/models/campaign.ts
@@ -1,10 +1,16 @@
 export interface Campaign {
+  id?: number;
   name: string;
-  createdAt: Date;
-  creatorId: string;
+  creatorId?: string;
   pageId: string;
+  createdAt?: Date;
   totalMessages: number;
   successMessages?: number;
   filteredBy?: string;
-  status: string
+  status?: string
+}
+
+export interface CampaignUpdate {
+  id: number;
+  [key: string]: string | number;
 }
