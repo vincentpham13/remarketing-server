0cae9a5b8c351b2cc35e81102ef61514f06543ab
Cuong <cuong@eq-centric.com>
2021-02-01 20:42:44 +0700

    Finish implement

---
+++ src/migrations/20210201171132_add_fields_to_campage_table.ts
@@ -0,0 +1,17 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.raw(`
+        alter table public.campaign
+        add message text not null,
+        add member_ids text[] not null,
+        add started_at timestamp without time zone DEFAULT now();
+    `)
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    
+}
+
